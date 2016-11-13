f1bd1e2bbfaa0ee009c2f288d95aea3e18e7418f
khemroat <khemroat@gmail.com>
2016-11-12 22:34:51 -0600

    initialize project spring hvp

---
+++ src/main/java/edu/mum/cs/cs544/spring/hvp/SpringHvpApplication.java
@@ -0,0 +1,12 @@
+package edu.mum.cs.cs544.spring.hvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHvpApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/mum/cs/cs544/spring/hvp/SpringHvpApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.mum.cs.cs544.spring.hvp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringHvpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
